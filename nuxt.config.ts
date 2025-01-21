ffff1a913e398f08818d3e865e13c6508d9aeb78
emmett hoolahan <gerhoolahan@icloud.com>
2025-01-21 15:00:15 +0800

    Add compatibility date and fix Netlify config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
+  compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
+  nitro: {
+    preset: 'netlify'
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
-  },
-  nitro: {
-    preset: 'netlify-edge'
   }
 })
